fa5a5d303cccb04426de71bc4ae7d8fe422ec9eb
Alan Sangma <asangma@esri.com>
2015-10-14 10:26:58 -0700

    double with earmark

--- pan-rotate-toggle/js/nav.js
+++ pan-rotate-toggle/js/nav.js
@@ -8,12 +8,16 @@ function getNav(uri) {
     jQuery.get(uri, function(data, textStatus, xhr) {
       var window_w = jQuery(window).width();
       jQuery('.navbar-nav').prepend(data);
-      
+        var curr_found = false;
         jQuery('.nav li').each(function(index, elem) {
             var href = jQuery(elem).children('a').attr('href'); 
             if (href == page) {
                 jQuery(elem).addClass('active');
+                curr_found = true;
             }
         });
+        if(!curr_found) {
+          jQuery('.nav li').last().addClass('active');
+        }
   });
 }
