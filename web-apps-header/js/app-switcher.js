1b937b3692b0ec31dd061ac91057aea9edb813f1
Alan Sangma <asangma@esri.com>
2016-09-30 09:54:45 -0700

    nice

--- web-apps-header/js/app-switcher.js
+++ web-apps-header/js/app-switcher.js
@@ -5,48 +5,50 @@ var apps = [
         name: "Marketplace",
         image: "images/product-icons/marketplace-48.png"
       },*/
-    {
+    /*{
         name: "Developers",
         letters: "D"
-    }, {
+    }, */{
         name: "Insights",
         image: "images/product-icons/Insights48.png",
         appIcon: "images/app-logos/insights-opt.svg",
         color: "#FF9F33"
-    }, {
+    }, /*{
         name: "GeoPlanner",
         letters: "GeoP"
-    }, {
+    }, *//*{
         name: "Business Analyst",
         image: "images/product-icons/BAO_Android_Launch_48.png"
-    }, {
+    }, *//*{
         name: "Community Analyst",
         letters: "CA"
-    }, {
+    }, */{
         name: "Workforce",
         image: "images/product-icons/workforce_android_48.png",
         appIcon: "images/app-logos/workforce-opt.svg",
         color: "#E04F1D"
     }, {
         name: "Survey123",
-        image: "images/product-icons/Survey123_Windows_48.png"
+        image: "images/product-icons/Survey123_Windows_48.png",
+        appIcon: "images/app-logos/survey123-opt.svg",
+        color: "#50AD4E"
     },
     // {
     //   name: "AppStudio",
     //   image: "images/product-icons/"
     // },
-    {
+    /*{
         name: "Open Data",
         letters: "Open"
-    }, {
+    },*/ {
         name: "Operations Dashboard",
         image: "images/product-icons/Operations_Dashboard_48.png",
         appIcon: "images/app-logos/operations-dashboard-opt.svg",
         color: "#FBB400"
-    }, {
+    }/*, {
         name: "Story Maps",
         letters: "SM"
-    }
+    }*/
     /*, {
         name: "Some App",
         letters: "SA"
@@ -67,7 +69,7 @@ jQuery(document).ready(function() { // You wish you could use jQuery
     ]
     jQuery.each(apps, function(index, app) {
         var color = colors[i];
-        var node_str = '<li class="app-module" id="app-no-' + index + '" role="menuitem" tabindex="0" data-app-icon="' + app.appIcon + '" data-color="' + app.color + '">'; // open li
+        var node_str = '<li class="app-module" id="app-no-' + index + '" role="menuitem" tabindex="0" data-app-name="' + app.name + '" data-app-icon="' + app.appIcon + '" data-color="' + app.color + '">'; // open li
         node_str += '<div class="app-icon-container ' + color + '">'; // open app-icon-container
         if (app.image) {
             node_str += '<div class="app-icon"><img src="' + app.image + '" alt=""/></div>'; // app-icon
@@ -88,19 +90,22 @@ jQuery(document).ready(function() { // You wish you could use jQuery
     jQuery('.app-switcher-trigger').on('click', function(e) {
         e.preventDefault();
         jQuery('body').toggleClass('app-switcher--visible');
-        console.log(jQuery(this));
-        if (jQuery('body').hasClass('app-switcher--visible')) {
-            jQuery('.app-switcher').css('left', (jQuery(this).position().left - jQuery('.app-switcher').width() + 75));
-        }
+        // console.log(jQuery(this));
+        // if (jQuery('body').hasClass('app-switcher--visible')) {
+        //     jQuery('.app-switcher').css('left', (jQuery(this).position().left - jQuery('.app-switcher').width() + 75));
+        // }
         var tabindex = jQuery('body').hasClass('app-switcher--visible') ? '0' : '-1';
         jQuery('.app-module').attr('tabindex', tabindex);
     });
     jQuery('.app-module').on('click', function(event){
+      jQuery('body').toggleClass('app-switcher--visible');
       var icon = jQuery(this).attr('data-app-icon');
       var color = jQuery(this).attr('data-color');
+      var app = jQuery(this).attr('data-app-name');
       if(icon !== "undefined") {
         jQuery('.app-header__app-icon-image').attr('src', icon);
         jQuery('.app-header').css('border-top-color', color);
       }
+      jQuery('.app-header__app-name').html(app);
     });
 });
--- web-apps-header/js/app.js
+++ web-apps-header/js/app.js
@@ -8,7 +8,7 @@ var appWrapper;
 function init() {
   // console.log('init');
   appWrapper = jQuery('.app-wrapper');
-  jQuery('.app-item').on('click', function(event){
+  jQuery('.in-item-toggle').on('click', function(event){
     event.preventDefault();
     appWrapper.toggleClass('in-item');
   });
